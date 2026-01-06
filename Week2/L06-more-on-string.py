963040333335b5b6da2e39eb3653f113fcdccbd3
Santanu Mandal <santanumandal5623@gmail.com>
2026-01-06 19:48:13 +0530

    d

--- Week2/L06-more-on-string.py
+++ Week2/L06-more-on-string.py
@@ -1,2 +1,10 @@
-print("input =")
-print("inside the practical value of pi is approximately 3.14")
+alpha="abcdefghijklmnopqrstuvwxyz"
+i=0
+print(alpha[i])
+print(alpha[i+1])
+print(alpha[i+2])
+print(alpha[i+3])
+# if i write alpha[i+26] it will give error as index out of range
+# there is a way to solve this problem using modulus operator
+print("using modulus operator")
+print(alpha[(i+26)%26])  # Output: a
