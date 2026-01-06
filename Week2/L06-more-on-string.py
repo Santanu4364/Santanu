6c3afd98a0c7e5df8184b1cc83788afd199e0ff6
Santanu Mandal <santanumandal5623@gmail.com>
2026-01-06 20:41:47 +0530

    d

--- Week2/L06-more-on-string.py
+++ Week2/L06-more-on-string.py
@@ -7,4 +7,14 @@ print(alpha[i+3])
 # if i write alpha[i+26] it will give error as index out of range
 # there is a way to solve this problem using modulus operator
 print("using modulus operator")
-print(alpha[(i+26)%26])  # Output: a
+i=25
+print(alpha[(i+1)%26])  # Output: a
+print(alpha[(i+2)%26])  # Output: b
+print(alpha[(i+3)%26])  # Output: c
+#so using modulus operator we can wrap around the index
+print("example problem")
+s="india"
+#i expext to output tvebstie
+t=''
+
+print(alpha[(((alpha.index(s[0]))+1)%26)])
