a417b6004630117078f6031f3f3f576120fadf98
Santanu Mandal <santanumandal5623@gmail.com>
2026-01-10 18:26:53 +0530

    d

--- Week2/L08-if-else-else-if.py
+++ Week2/L08-if-else-else-if.py
@@ -1,6 +1,18 @@
+# Check if a number is even or odd
 print("Enter a number: ")
 num=int(input())
 if(num % 2 == 0):
     print("Even")
 else:
     print("Odd")
+
+
+num=int(input("Enter a number: "))
+if(num%5==0):
+    if(num%10==0):
+        print("last digit is 0")
+    else:
+        print("last digit is 5")
+else:
+    print("number is from other category")
+
