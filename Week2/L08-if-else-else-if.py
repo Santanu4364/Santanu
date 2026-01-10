3591bf727ba653557acbecfee3fcec9cac6a2079
Santanu Mandal <santanumandal5623@gmail.com>
2026-01-10 19:16:57 +0530

    d

--- Week2/L08-if-else-else-if.py
+++ Week2/L08-if-else-else-if.py
@@ -18,13 +18,15 @@ else:
 
 #check grade based on marks
 marks=int(input("Enter your marks: "))
-if marks>=90:
+if (marks>=90):
     print("Grade A")
-if marks>=80 and marks<90:
+if (marks>=80 and marks<90):
     print("Grade B")
-if marks>=70 and marks<80:
+if (marks>=70 and marks<80):
     print("Grade C")
-if marks >=60 and marks<70:
+if (marks>=60 and marks<70):
     print("Grade D")
+if (marks<60):
+    print("E")
 else:
-    print("Grade E")
+    print("invalid input")
