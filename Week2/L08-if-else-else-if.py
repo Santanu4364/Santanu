40a2161b65964d81a2ddadcaa08b03f8eff4eba8
Santanu Mandal <santanumandal5623@gmail.com>
2026-01-10 19:08:13 +0530

    f

--- Week2/L08-if-else-else-if.py
+++ Week2/L08-if-else-else-if.py
@@ -6,7 +6,7 @@ if(num % 2 == 0):
 else:
     print("Odd")
 
-
+# check the last digit of a number
 num=int(input("Enter a number: "))
 if(num%5==0):
     if(num%10==0):
@@ -15,3 +15,16 @@ if(num%5==0):
         print("last digit is 5")
 else:
     print("number is from other category")
+
+#check grade based on marks
+marks=int(input("Enter your marks: "))
+if marks>=90:
+    print("Grade A")
+if marks>=80 and marks<90:
+    print("Grade B")
+if marks>=70 and marks<80:
+    print("Grade C")
+if marks >=60 and marks<70:
+    print("Grade D")
+else:
+    print("Grade E")
