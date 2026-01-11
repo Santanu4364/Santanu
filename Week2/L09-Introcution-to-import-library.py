3ca603d0bddf54f88f05d52febb9fa613cb20bfe
Santanu Mandal <santanumandal5623@gmail.com>
2026-01-12 00:09:00 +0530

    d

--- Week2/L09-Introcution-to-import-library.py
+++ Week2/L09-Introcution-to-import-library.py
@@ -10,3 +10,11 @@ if coin<0.5:
     print("Heads")
 else:
     print("Tails")
+
+#lets simulate a dice roll 6 sided dice
+dice = random.randrange(1,7)
+print(dice)
+#sumation of two dice
+dice1=random.randrange(1,7)
+dice2=random.randrange(1,7)
+print(dice1+dice2)
