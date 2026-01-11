1870a6eb9e4b7cf9dea12fa6c89d1b9c29504479
Santanu Mandal <santanumandal5623@gmail.com>
2026-01-12 00:04:49 +0530

    d

--- Week2/L09-Introcution-to-import-library.py
+++ Week2/L09-Introcution-to-import-library.py
@@ -1,2 +1,12 @@
 import math
 print(math.log(10))  # prints the logarithm of 100
+print(math.sqrt(25))
+print(math.cos(45))
+
+#lets simulate the toos of a coin
+import random
+coin = random.random()
+if coin<0.5:
+    print("Heads")
+else:
+    print("Tails")
