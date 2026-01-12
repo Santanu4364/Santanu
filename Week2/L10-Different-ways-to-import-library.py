9d4c159bd0bf6737fbb571798bf940f7173848ba
Santanu Mandal <santanumandal5623@gmail.com>
2026-01-12 17:30:37 +0530

    d

--- Week2/L10-Different-ways-to-import-library.py
+++ Week2/L10-Different-ways-to-import-library.py
@@ -1,18 +1,18 @@
-'''import calendar
+import calendar
 
 print(calendar.month(2024, 8))
 #print entire 2026 year calendar
 print(calendar.calendar(2026))
-'''
-'''
+
+#different ways to import specific functions
 from calendar import *
 print(calendar(2026))
 print(month(2025, 10))
-'''
-'''from calendar import calendar, month
+#import specific functions
+from calendar import calendar, month
 print(calendar(2024))
-print(month(2025, 10))'''
-
+print(month(2025, 10))
+#import with alias
 import calendar as c
 print(c.month(2024, 8))
 
