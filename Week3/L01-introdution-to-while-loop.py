18b8ed6e82ead9204d4ab90f7919dc51b31d1f6d
Santanu Mandal <santanumandal5623@gmail.com>
2026-01-12 18:03:18 +0530

    d

---
+++ Week3/L01-introdution-to-while-loop.py
@@ -0,0 +1,24 @@
+# make a second cahnce using if else loop
+'''print("when india got independance?: ")
+year=int(input())
+if year==1947:
+    print("Hip Hip Hurrey. You get it!")
+else:
+    print("cammon you dont know this")
+    print("it's okey lets try again")
+    print('what is the independance year of india?: ')
+    year=int(input())
+    if year==1947:
+        print("you get it")
+    else:
+        print("you are wrong again...")'''
+
+# lets write a code that allow as many attempt as the user want.
+# The code wil end when it will get the corrwct answer.
+print("when did india get independance: ")
+year=int(input())
+
+while(year!=1947):
+      print("you get it wrong")
+      year=int(input())
+print("Wowwwwwww you get it right")
