c8c9f0236113447b371ef9026346851e1c9bd8a3
Santanu Mandal <santanumandal5623@gmail.com>
2026-01-14 13:02:55 +0530

    d

--- Week3/L03-Tutorual-on-while-loop.py
+++ Week3/L03-Tutorual-on-while-loop.py
@@ -14,3 +14,32 @@ while(num>0):
     rev=rev*10+rem
     num=num//10
 print("the reverse of the number is: ", rev)
+
+#reverse of a negative number with sign
+n=int(input("number: "))
+absNum=abs(n)
+rev=0
+while(absNum>0):
+    r=absNum%10
+
+    rev=rev*10+r
+    absNum=absNum//10
+if (n>0):
+    print(rev)
+else:
+    print(rev-2*rev)
+
+#find the number is palindrome or not
+n=int(input("number: "))
+absnum=abs(n)
+rev=0
+while(absnum>0):
+    rem=absnum%10
+    rev=rev*10+rem
+    absnum=absnum//10
+if(n<0):
+    rev=rev-rev*2
+if(rev==n):
+    print("palindrom")
+else:
+    print("not a palindrom")
