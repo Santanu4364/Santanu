cf1a45cc86b5ea57ace68474840519906567f70a
Santanu4364 <santanumandal841@gmail.com>
2026-03-30 13:57:24 +0530

    Fixed Week3 submodule issue

---
+++ Week3/L05-addition-of-numbers-using-for-loop.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#addition of first 20 integer number using for loop
+ans=0
+for i in range (20):
+    ans=ans+i
+print(ans)
+
+#addition of any numbers of integers using for loop
+ans=0
+n=int(input("enter the number: "))
+for i in range (n):
+    ans=ans+i
+print(ans)
+
